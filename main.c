97afef37299d0b775e07aedf97f3f57f970ad454
Nevostrius <stefanv73@web.de>
2019-06-23 21:57:39 +0200

    initial release files

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <memory.h>
+#define START 3
+#include "structs.h"
+#include "products.h"
+
+
+int main()
+{
+    handle national_products;
+
+    choose(&national_products);
+}
